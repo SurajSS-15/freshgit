f1781a08ebb3d69fbfb8c6f5344c181957ddec48
SurajSS-15 <frisbee3494@gmail.com>
2024-09-24 14:29:47 +0530

    text

--- app/src/main/java/com/example/freshgit/MainActivity.kt
+++ app/src/main/java/com/example/freshgit/MainActivity.kt
@@ -3,11 +3,17 @@ package com.example.freshgit
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.freshgit.ui.theme.FreshGitTheme
@@ -29,7 +35,24 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun Hello() {
+    Scaffold(
+        topBar = {},
+        bottomBar = {},
+        content = {
+            Surface(modifier = Modifier
+                .fillMaxSize()
+                .padding(it)) {
+                Column(modifier = Modifier.fillMaxSize(), verticalArrangement = Arrangement.Center, horizontalAlignment = Alignment.CenterHorizontally) {
+                    Text(text = "DEV")
+                    
+                }
+                
+                
+            }
+        }
+    )
     
 }
